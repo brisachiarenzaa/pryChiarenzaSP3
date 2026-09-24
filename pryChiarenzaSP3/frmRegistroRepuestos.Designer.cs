a14842057a27d7540917c4e66480f4c8a1f17938
brisachiarenzaa <brisachiarenza1405@gmail.com>
2026-09-24 09:39:30 -0300

    24-9 / Gestion de repuestos (interfaz y codigo) listos

--- pryChiarenzaSP3/frmRegistroRepuestos.Designer.cs
+++ pryChiarenzaSP3/frmRegistroRepuestos.Designer.cs
@@ -39,53 +39,56 @@
             this.rdbNacional = new System.Windows.Forms.RadioButton();
             this.rdbImportado = new System.Windows.Forms.RadioButton();
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lsbDatos = new System.Windows.Forms.ListBox();
+            this.gpbOrigen = new System.Windows.Forms.GroupBox();
+            this.gpbOrigen.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
-            this.lblMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMarca.Location = new System.Drawing.Point(47, 55);
+            this.lblMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarca.Location = new System.Drawing.Point(14, 56);
             this.lblMarca.Name = "lblMarca";
-            this.lblMarca.Size = new System.Drawing.Size(145, 16);
+            this.lblMarca.Size = new System.Drawing.Size(48, 16);
             this.lblMarca.TabIndex = 0;
-            this.lblMarca.Text = "Marca del repuesto:";
+            this.lblMarca.Text = "Marca:";
             // 
             // lblOrigen
             // 
             this.lblOrigen.AutoSize = true;
-            this.lblOrigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOrigen.Location = new System.Drawing.Point(44, 95);
+            this.lblOrigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrigen.Location = new System.Drawing.Point(13, 92);
             this.lblOrigen.Name = "lblOrigen";
-            this.lblOrigen.Size = new System.Drawing.Size(148, 16);
+            this.lblOrigen.Size = new System.Drawing.Size(50, 16);
             this.lblOrigen.TabIndex = 1;
-            this.lblOrigen.Text = "Origen del repuesto:";
+            this.lblOrigen.Text = "Origen:";
             // 
             // cmbMarca
             // 
             this.cmbMarca.FormattingEnabled = true;
-            this.cmbMarca.Items.AddRange(new object[] {
-            "P",
-            "F",
-            "R"});
-            this.cmbMarca.Location = new System.Drawing.Point(198, 50);
+            this.cmbMarca.Location = new System.Drawing.Point(100, 55);
             this.cmbMarca.Name = "cmbMarca";
             this.cmbMarca.Size = new System.Drawing.Size(81, 21);
             this.cmbMarca.TabIndex = 2;
+            this.cmbMarca.SelectedIndexChanged += new System.EventHandler(this.cmbMarca_SelectedIndexChanged);
             // 
             // lblNumRepuesto
             // 
             this.lblNumRepuesto.AutoSize = true;
-            this.lblNumRepuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumRepuesto.Location = new System.Drawing.Point(36, 130);
+            this.lblNumRepuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumRepuesto.Location = new System.Drawing.Point(13, 175);
             this.lblNumRepuesto.Name = "lblNumRepuesto";
-            this.lblNumRepuesto.Size = new System.Drawing.Size(156, 16);
+            this.lblNumRepuesto.Size = new System.Drawing.Size(58, 16);
             this.lblNumRepuesto.TabIndex = 4;
-            this.lblNumRepuesto.Text = "Numero del repuesto:";
+            this.lblNumRepuesto.Text = "Numero:";
             // 
             // txtNumRepuesto
             // 
-            this.txtNumRepuesto.Location = new System.Drawing.Point(198, 126);
+            this.txtNumRepuesto.Location = new System.Drawing.Point(100, 171);
             this.txtNumRepuesto.Name = "txtNumRepuesto";
             this.txtNumRepuesto.Size = new System.Drawing.Size(93, 20);
             this.txtNumRepuesto.TabIndex = 5;
@@ -93,73 +96,123 @@
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescripcion.Location = new System.Drawing.Point(7, 169);
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(13, 242);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(185, 16);
+            this.lblDescripcion.Size = new System.Drawing.Size(82, 16);
             this.lblDescripcion.TabIndex = 6;
-            this.lblDescripcion.Text = "Descripcion del repuesto:";
+            this.lblDescripcion.Text = "Descripcion:";
             // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(198, 165);
+            this.txtDescripcion.Location = new System.Drawing.Point(100, 238);
             this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(165, 20);
+            this.txtDescripcion.Size = new System.Drawing.Size(131, 20);
             this.txtDescripcion.TabIndex = 7;
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrecio.Location = new System.Drawing.Point(45, 205);
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(13, 211);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(147, 16);
+            this.lblPrecio.Size = new System.Drawing.Size(49, 16);
             this.lblPrecio.TabIndex = 8;
-            this.lblPrecio.Text = "Precio del repuesto:";
+            this.lblPrecio.Text = "Precio:";
             // 
             // rdbNacional
             // 
             this.rdbNacional.AutoSize = true;
             this.rdbNacional.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdbNacional.Location = new System.Drawing.Point(200, 94);
+            this.rdbNacional.Location = new System.Drawing.Point(26, 14);
             this.rdbNacional.Name = "rdbNacional";
-            this.rdbNacional.Size = new System.Drawing.Size(84, 19);
+            this.rdbNacional.Size = new System.Drawing.Size(74, 19);
             this.rdbNacional.TabIndex = 9;
             this.rdbNacional.TabStop = true;
-            this.rdbNacional.Text = "NACIONAL";
+            this.rdbNacional.Text = "Nacional";
             this.rdbNacional.UseVisualStyleBackColor = true;
             // 
             // rdbImportado
             // 
             this.rdbImportado.AutoSize = true;
             this.rdbImportado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdbImportado.Location = new System.Drawing.Point(285, 94);
+            this.rdbImportado.Location = new System.Drawing.Point(155, 14);
             this.rdbImportado.Name = "rdbImportado";
-            this.rdbImportado.Size = new System.Drawing.Size(97, 19);
+            this.rdbImportado.Size = new System.Drawing.Size(81, 19);
             this.rdbImportado.TabIndex = 10;
             this.rdbImportado.TabStop = true;
-            this.rdbImportado.Text = "IMPORTADO";
+            this.rdbImportado.Text = "Importado";
             this.rdbImportado.UseVisualStyleBackColor = true;
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(95, 19);
+            this.lblTitulo.Location = new System.Drawing.Point(63, 19);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(196, 18);
             this.lblTitulo.TabIndex = 11;
             this.lblTitulo.Text = "VENTA DE REPUESTOS";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(12, 430);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(105, 42);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(147, 430);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(106, 42);
+            this.btnRegistrar.TabIndex = 14;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lsbDatos
+            // 
+            this.lsbDatos.FormattingEnabled = true;
+            this.lsbDatos.Location = new System.Drawing.Point(17, 309);
+            this.lsbDatos.Name = "lsbDatos";
+            this.lsbDatos.Size = new System.Drawing.Size(237, 95);
+            this.lsbDatos.TabIndex = 15;
+            // 
+            // gpbOrigen
+            // 
+            this.gpbOrigen.Controls.Add(this.rdbImportado);
+            this.gpbOrigen.Controls.Add(this.rdbNacional);
+            this.gpbOrigen.Location = new System.Drawing.Point(4, 120);
+            this.gpbOrigen.Name = "gpbOrigen";
+            this.gpbOrigen.Size = new System.Drawing.Size(249, 45);
+            this.gpbOrigen.TabIndex = 16;
+            this.gpbOrigen.TabStop = false;
+            // 
             // frmRepuestos
             // 
+            this.AcceptButton = this.btnRegistrar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(392, 327);
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(279, 510);
+            this.Controls.Add(this.gpbOrigen);
+            this.Controls.Add(this.lsbDatos);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lblTitulo);
-            this.Controls.Add(this.rdbImportado);
-            this.Controls.Add(this.rdbNacional);
             this.Controls.Add(this.lblPrecio);
             this.Controls.Add(this.txtDescripcion);
             this.Controls.Add(this.lblDescripcion);
@@ -169,7 +222,10 @@
             this.Controls.Add(this.lblOrigen);
             this.Controls.Add(this.lblMarca);
             this.Name = "frmRepuestos";
-            this.Text = "Form1";
+            this.Text = "Gestion de Repuestos";
+            this.Load += new System.EventHandler(this.frmRepuestos_Load);
+            this.gpbOrigen.ResumeLayout(false);
+            this.gpbOrigen.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,6 +244,11 @@
         private System.Windows.Forms.RadioButton rdbNacional;
         private System.Windows.Forms.RadioButton rdbImportado;
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListBox lsbDatos;
+        private System.Windows.Forms.GroupBox gpbOrigen;
     }
 }
 
---
+++ pryChiarenzaSP3/frmRegistroRepuestos.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryChiarenzaSP3
+{
+    public partial class frmRepuestos : Form
+    {
+
+        // DECLARACION DE VARIABLES GLOBALES
+
+        // Array de 1 dimension - vector - sin elementos
+        string[] vecRegistros;
+
+        // Array de 2 dimensiones - matriz - sin elementos
+        string[,] matRegistros;
+
+        // Array de 1 dimension - vector - sin elementos
+        string[] vecRepuestos = new string[3];
+
+        // Array de 2 dimensiones - matriz
+        string[,] matRepuestos = new string[2, 2];
+
+        int indiceRegistros = 0;
+
+        public frmRepuestos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRepuestos_Load(object sender, EventArgs e)
+        {
+            cmbMarca.Items.Add("P");
+            cmbMarca.Items.Add("F");
+            cmbMarca.Items.Add("R");
+        }
+
+        private void cmbMarca_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string varMarca = cmbMarca.Text;
+            string varOrigen;
+
+            // Estoy registrando como texto a Marca. lo cargado se guardara en la cmb
+
+            if (rdbNacional.Checked == true)
+            {
+                varOrigen = "Nacional";
+
+            }
+
+            else
+            {
+
+                varOrigen = "Importado";
+
+            }
+
+
+
+            lsbDatos.Items.Add(varMarca + "  " + varOrigen);
+
+            // Grabar en el VECTOR - Array de 1 dimension
+
+            vecRepuestos[indiceRegistros] =  varMarca + " " + varOrigen;
+            indiceRegistros++;
+
+            if (indiceRegistros == vecRepuestos.Length)
+            {
+                btnRegistrar.Enabled = false;
+                MessageBox.Show("Se ha completado el registro de repuestos");
+            }
+
+
+            // Añadir los elementos del vector a la lista
+
+            // Bucle repetitivo p/ recorrer el vector y añadir sus elementos a la lista
+            for (int indice = 0; indice < vecRepuestos.Length; indice++)
+            {
+                lsbDatos.Items.Add(vecRepuestos[indice]);
+            }
+
+        }
+
+    }
+}
+
